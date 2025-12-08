b908c815ebf1ac4ed69d61d3f3a1820d6a6e8435
sultangithub04 <sult_04@yahoo.com>
2025-12-08 16:13:25 +0600

    updated

--- src/app/(commonLayout)/layout.tsx
+++ src/app/(commonLayout)/layout.tsx
@@ -1,5 +1,7 @@
 import PublicFooter from "@/components/shared/PublicFooter";
 import PublicNavbar from "@/components/shared/PublicNavbar";
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
 
 const CommonLayout = ({ children } : { children: React.ReactNode }) => {
     return (
